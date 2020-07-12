03474f6526ae2229dba7ba6d503be2d321af8fe1
paraselene92 <paraselene92@gmail.com>
2020-07-12 22:31:40 +0900

    for windows terminal 1.0

--- fish/functions/wallpicopen.fish
+++ fish/functions/wallpicopen.fish
@@ -1,5 +1,5 @@
 function wallpicopen
-  set WTJSON_FILE (string join "" $WTJSON_DIR "profiles.json")
+  set WTJSON_FILE (string join "" $WTJSON_DIR "settings.json")
   set pic_path (cat $WTJSON_FILE | jq -r .profiles[2].backgroundImage)
   set pic_path (string replace -a \\\\ \\ $pic_path)
 
--- fish/functions/wallreturn.fish
+++ fish/functions/wallreturn.fish
@@ -1,6 +1,6 @@
 function wallreturn
-  set WTJSON_FILE (string join "" $WTJSON_DIR "profiles.json")
-  set WTJSON_OLDFILE (string join "" $WTJSON_DIR "profiles.json.old")
+  set WTJSON_FILE (string join "" $WTJSON_DIR "settings.json")
+  set WTJSON_OLDFILE (string join "" $WTJSON_DIR "settings.json.old")
 
   mv -f $WTJSON_OLDFILE $WTJSON_FILE
 end
