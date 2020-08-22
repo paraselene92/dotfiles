f0de8a226c08fd742c25fccbdd6347555a24bd86
paraselene92 <paraselene92@gmail.com>
2020-08-22 21:28:52 +0900

    mod: ssh-agent

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,18 @@ set WTJSON_DIR "/mnt/c/Users/$WC_USERNAME/AppData/Local/Packages/Microsoft.Windo
 
 # starship init
 starship init fish | source
+
+# invoke fish-ssh-agent
+fish_ssh_agent
+
+# ssh-add
+set keys ~/.ssh/keys/id_rsa ~/.ssh/keys/github
+if test -f (which keychain)
+  for i in $keys;
+    eval (keychain --eval --quiet --agents ssh $i)
+  end
+end
+
+# ssh-add (pemonly)
+ssh-add ~/.ssh/keys/site-lightsail01.pem
+
