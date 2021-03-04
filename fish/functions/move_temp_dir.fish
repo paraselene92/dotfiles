bceb6bbd5cfecfa9aa26280236b9eb7bf56a30ee
paraselene92 <paraselene92@gmail.com>
2021-03-04 23:04:12 +0900

    fix: tweaks

--- fish/functions/move_temp_dir.fish
+++ fish/functions/move_temp_dir.fish
@@ -1,5 +1,5 @@
 function move_temp_dir
-    set -x today (date | awk -F" " '{ print $6,$2,$3 }' | tr " " "-")
+    set -x today (date -I)
     if not test -d /tmp/$today
         mkdir /tmp/$today
     end
