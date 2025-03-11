e71c9435cfa502d075ceeb161c11b23bb4aa808e
paraselene92 <paraselene92@gmail.com>
2025-03-11 23:00:51 +0900

    mod: gitsigns shortcut

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,11 @@ vim.keymap.set("n", "<S-a>", "^", { noremap = true, silent = true })
 vim.keymap.set("n", "<S-l>", "$", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-e>", ":Neotree<CR>", { noremap = true, silent = true })
 -- vim.keymap.set("n", "<C-e>", ":NvimTreeOpen<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>hd", function()
+	require("gitsigns").diffthis()
+end, { desc = "GitSigns [h]unk [d]iff" })
+
+vim.keymap.set("n", "<leader>hi", function()
+	require("gitsigns").preview_hunk_inline()
+end, { desc = "GitSigns [h]unk preview [i]nline" })
